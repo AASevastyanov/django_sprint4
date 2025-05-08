f08c44530aee40a0820c0f53eedc68884e3bb5eb
Aleksey Sevastyanov <sevastyanoff.aloshade@yandex.ru>
2025-05-08 22:28:19 +0300

    Попытка

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -3,8 +3,9 @@ from django.contrib import admin
 from django.urls import path, include
 from users import views as users_views
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('profile/<str:username>/', users_views.profile, name='profile'),
+    path('admin/', admin.site.urls),
+    path('auth/', include('django.contrib.auth.urls')),
     path('', include('blog.urls', namespace='blog')),
     path('', include('pages.urls', namespace='pages')),
 ]
--- blogicum/users/views.py
+++ blogicum/users/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth import get_user_model
+from django.core.paginator import Paginator
+from blog.models import Post
+from django.utils import timezone
+
+User = get_user_model()
 
 
 def profile(request, username):
-    return render(request, 'users/profile.html', {'username': username})
+    user = get_object_or_404(User, username=username)
+
+    posts = Post.objects.filter(author=user).select_related('category')
+    if request.user != user:
+        posts = posts.filter(
+            is_published=True,
+            pub_date__lte=timezone.now(),
+            category__is_published=True,
+        )
+
+    paginator = Paginator(posts, 10)
+    page_obj = paginator.get_page(request.GET.get('page'))
+
+    return render(request, 'users/profile.html', {
+        'page_obj': page_obj,
+        'profile_user': user,
+    })
